dfcd9db36d7832560dad64714583fad61e2271da
krokofant <krokofant@users.noreply.github.com>
2019-03-26 00:34:59 +0100

    More verbose

--- GetDependencies.ps1
+++ GetDependencies.ps1
@@ -5,11 +5,10 @@ $prettierVersion = @($env:PRETTIERVERSION, "1.16.4") | Where-Object { -not ($_ -
 if(-not (Test-Path "$PSScriptRoot\temp")) { mkdir "$PSScriptRoot\temp" }
 
 ### NodeJS
-Write-Host "Downloading nodejs"
+Write-Host "Downloading nodejs $nodeVersion"
 $nodeUrl = "https://nodejs.org/dist/$nodeVersion/node-$nodeVersion-win-x86.zip"
-$nodeSave = "nodejs.zip"
 $client = new-object System.Net.WebClient
-$client.DownloadFile($nodeUrl, $nodeSave)
+$client.DownloadFile($nodeUrl, "nodejs.zip")
 Write-Host "Extracting nodejs"
 # Expand-Archive -Path .\nodejs.zip -DestinationPath .\temp
 .\7za.exe x -otemp .\nodejs.zip -r
@@ -18,7 +17,7 @@ Get-ChildItem .\temp\node-v* | Rename-Item -NewName node
 ### Prettier
 Write-Host "Listing prettier versions:"
 npm view prettier versions
-Write-Host "Downloading prettier"
+Write-Host "Downloading prettier $prettierVersion"
 npm pack prettier@"$prettierVersion"
 tar xzvf (Get-ChildItem .\prettier-*) -C temp
 Rename-Item .\temp\package -NewName prettier
