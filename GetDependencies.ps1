aa30c33dcd211101c0045e8d8e6c058efe070303
krokofant <krokofant@users.noreply.github.com>
2019-03-25 23:36:49 +0100

    Hello world

---
+++ GetDependencies.ps1
@@ -0,0 +1,24 @@
+$xml = ([xml](Get-Content .\nodeenv.nuspec))
+$nodeVersion = $xml.package.metadata.nodeVersion
+$prettierVersion = $xml.package.metadata.prettierVersion
+
+if(-not (Get-Item temp)) { mkdir temp }
+
+### NodeJS
+Write-Host "Downloading nodejs"
+$nodeUrl = "https://nodejs.org/dist/latest-v10.x/node-$nodeVersion-win-x86.zip"
+$nodeSave = "nodejs.zip"
+$client = new-object System.Net.WebClient
+$client.DownloadFile($nodeUrl, $nodeSave)
+7z x -otemp .\nodejs.zip -r
+Get-ChildItem .\temp\node-v* | Rename-Item -NewName node
+
+### Prettier
+Write-Host "Listing prettier versions:"
+npm view prettier versions
+Write-Host "Downloading prettier"
+npm pack prettier@"$prettierVersion"
+tar xzvf (Get-ChildItem .\prettier-*) -C temp
+Rename-Item .\temp\package -NewName prettier
+Move-Item .\temp\prettier .\temp\node\node_modules\
+Copy-Item .\prettier.cmd .\temp\node\
