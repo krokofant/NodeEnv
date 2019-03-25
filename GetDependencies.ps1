3423397e78acf4560588f219972e9aaa72fb3d58
krokofant <krokofant@users.noreply.github.com>
2019-03-26 00:30:28 +0100

    Faster extract and fix nuspec

--- GetDependencies.ps1
+++ GetDependencies.ps1
@@ -1,6 +1,6 @@
 $xml = ([xml](Get-Content .\nodeenv.nuspec))
-$nodeVersion = $xml.package.metadata.nodeVersion
-$prettierVersion = $xml.package.metadata.prettierVersion
+$nodeVersion = @($env:NODEVERSION, "v10.15.3") | Where-Object { -not ($_ -eq $null) }
+$prettierVersion = @($env:PRETTIERVERSION, "1.16.4") | Where-Object { -not ($_ -eq $null) }
 
 if(-not (Test-Path "$PSScriptRoot\temp")) { mkdir "$PSScriptRoot\temp" }
 
@@ -10,7 +10,9 @@ $nodeUrl = "https://nodejs.org/dist/$nodeVersion/node-$nodeVersion-win-x86.zip"
 $nodeSave = "nodejs.zip"
 $client = new-object System.Net.WebClient
 $client.DownloadFile($nodeUrl, $nodeSave)
-Expand-Archive -Path .\nodejs.zip -DestinationPath .\temp
+Write-Host "Extracting nodejs"
+# Expand-Archive -Path .\nodejs.zip -DestinationPath .\temp
+.\7za.exe x -otemp .\nodejs.zip -r
 Get-ChildItem .\temp\node-v* | Rename-Item -NewName node
 
 ### Prettier
