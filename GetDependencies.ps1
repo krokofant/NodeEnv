26547edf9efc83410e3249af57d2beb112a18508
krokofant <krokofant@users.noreply.github.com>
2019-03-31 23:38:33 +0200

    Fix version references in nuspec

--- GetDependencies.ps1
+++ GetDependencies.ps1
@@ -3,23 +3,30 @@ $ErrorActionPreference = "Stop"
 $nodeVersion = @($env:NODEVERSION, "v10.15.3") | Where-Object { -not ($_ -eq $null) } | Select-Object -First 1
 $prettierVersion = @($env:PRETTIERVERSION, "1.16.4") | Where-Object { -not ($_ -eq $null) } | Select-Object -First 1
 
-if(-not (Test-Path "$PSScriptRoot\temp")) { mkdir "$PSScriptRoot\temp" }
+if (-not (Test-Path "$PSScriptRoot\temp")) { mkdir "$PSScriptRoot\temp" }
 
 ### NodeJS
 Write-Host "Downloading nodejs $nodeVersion"
 $nodeUrl = "https://nodejs.org/dist/$nodeVersion/node-$nodeVersion-win-x86.zip"
-$client = new-object System.Net.WebClient
+$client = New-Object System.Net.WebClient
 $client.DownloadFile($nodeUrl, "nodejs.zip")
 Write-Host "Extracting nodejs"
 # Expand-Archive -Path .\nodejs.zip -DestinationPath .\temp
-7z x -otemp .\nodejs.zip -r
+7z.exe x -otemp .\nodejs.zip -r
 Get-ChildItem .\temp\node-v* | Rename-Item -NewName node
 
 ### Prettier
 Write-Host "Downloading prettier $prettierVersion"
 $prettierUrl = "https://registry.npmjs.org/prettier/-/prettier-$prettierVersion.tgz"
 $client.DownloadFile($prettierUrl, "prettier.tgz")
-tar xzvf .\prettier.tgz -C temp
+tar.exe xzvf .\prettier.tgz -C temp
 Rename-Item .\temp\package -NewName prettier
 Move-Item .\temp\prettier .\temp\node\node_modules\
 Copy-Item .\prettier.cmd .\temp\node\
+
+# Update nuspec with dep version information
+(Get-Content .\nodeenv.nuspec).Replace(
+    '$nodeVersion$', $nodeVersion
+).Replace(
+    '$prettierVersion$', $prettierVersion
+) | Out-File .\temp\nodeenv.nuspec
--- build.ps1
+++ build.ps1
@@ -7,7 +7,7 @@ if ($env:TRAVIS_BUILD_NUMBER.Count -gt 0) {
     $version = "$version.$env:TRAVIS_BUILD_NUMBER"
 }
 
-nuget pack -Version $version -OutputDirectory build -NoPackageAnalysis
+nuget.exe pack .\temp\nodeenv.nuspec -BasePath .\ -Version $version -OutputDirectory build -NoPackageAnalysis
 $hash = (Get-FileHash ".\build\$id.$version.nupkg" -Algorithm SHA512).Hash.ToLower()
 $hash | Out-File ".\build\$id.$version.nupkg.sha512"
 Get-ChildItem .\build\*
