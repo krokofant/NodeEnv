11105d60050740fe12f070e23012f4d8611c9e4e
krokofant <krokofant@users.noreply.github.com>
2019-03-31 21:58:20 +0200

    Updates for travis

--- GetDependencies.ps1
+++ GetDependencies.ps1
@@ -1,4 +1,3 @@
-$xml = ([xml](Get-Content .\nodeenv.nuspec))
 $nodeVersion = @($env:NODEVERSION, "v10.15.3") | Where-Object { -not ($_ -eq $null) } | Select-Object -First 1
 $prettierVersion = @($env:PRETTIERVERSION, "1.16.4") | Where-Object { -not ($_ -eq $null) } | Select-Object -First 1
 
@@ -11,15 +10,14 @@ $client = new-object System.Net.WebClient
 $client.DownloadFile($nodeUrl, "nodejs.zip")
 Write-Host "Extracting nodejs"
 # Expand-Archive -Path .\nodejs.zip -DestinationPath .\temp
-.\7za.exe x -otemp .\nodejs.zip -r
+7z x -otemp .\nodejs.zip -r
 Get-ChildItem .\temp\node-v* | Rename-Item -NewName node
 
 ### Prettier
-Write-Host "Listing prettier versions:"
-npm view prettier versions
 Write-Host "Downloading prettier $prettierVersion"
-npm pack prettier@"$prettierVersion"
-tar xzvf (Get-ChildItem .\prettier-*) -C temp
+$prettierUrl = "https://registry.npmjs.org/prettier/-/prettier-$prettierVersion.tgz"
+$client.DownloadFile($prettierUrl, "prettier.tgz")
+tar xzvf .\prettier.tgz -C temp
 Rename-Item .\temp\package -NewName prettier
 Move-Item .\temp\prettier .\temp\node\node_modules\
 Copy-Item .\prettier.cmd .\temp\node\
--- build.ps1
+++ build.ps1
@@ -1,8 +1,11 @@
 $xml = ([xml](Get-Content .\nodeenv.nuspec))
 $id = $xml.package.metadata.id
-$version = $xml.package.metadata.version
+$version = (Get-Date -Format "yyyy.M.d")
+if ($env:TRAVIS_BUILD_NUMBER.Count -gt 0) {
+    $version = "$version.$env:TRAVIS_BUILD_NUMBER"
+}
 
-Invoke-Expression "nuget pack -OutputDirectory build"
-Copy-Item .\nodeenv.nuspec .\build
+nuget pack -Version $version -OutputDirectory build -NoPackageAnalysis
 $hash = (Get-FileHash ".\build\$id.$version.nupkg" -Algorithm SHA512).Hash.ToLower()
 $hash | Out-File ".\build\$id.$version.nupkg.sha512"
+Get-ChildItem .\build\*
